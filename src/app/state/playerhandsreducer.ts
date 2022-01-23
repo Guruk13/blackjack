f0f1442b9229ef1c1a58a19b3a3b8adcaa6608aa
meshtild <meshtild@meshManjaro>
2022-01-23 04:43:03 +0100

    dealing random card to player done without using third party library

--- src/app/state/playerhandsreducer.ts
+++ src/app/state/playerhandsreducer.ts
@@ -1,14 +1,24 @@
 import { createReducer, on } from '@ngrx/store';
+import { createImmerReducer } from 'ngrx-immer/store'
 import { addCard, dealCard, drawCard, createPlayers } from './pack.actions';
 import { PlayerHand, } from 'app/playerHands.model';
 import { state } from '@angular/animations';
 
 export const initialState: ReadonlyArray<PlayerHand> = [];
 
-export const playerHandsReducer = createReducer(
+export const playerHandsReducer = createImmerReducer(
   initialState,
   on(createPlayers, (state, { somePlayers }) => somePlayers),
-  on(dealCard, (state, { tempoplayer }) => ({ ...state,   })),
-  
+  on(dealCard, (state, { tempoplayer, cardToDeal }) => {
+    return {
+      ...state,
+      [tempoplayer.id] : {
+        id: tempoplayer.id,
+        hand: [...state[tempoplayer.id].hand, cardToDeal]
+      }
+    }
+  })
 
-);  
+
+
+);
