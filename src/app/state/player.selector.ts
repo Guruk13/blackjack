5a6d38251c32d32356ff1ebd97d02200571e69b7
meshtild <meshtild@meshManjaro>
2022-01-27 10:02:03 +0100

    Stackoverflow question asked, let us proceed to implementing smooth UI

--- src/app/state/player.selector.ts
+++ src/app/state/player.selector.ts
@@ -2,9 +2,21 @@ import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { Player } from '../models/player.model';
 import { PossessedCard } from '../models/possessedCards.model';
 
-export const selectPlayers =  createFeatureSelector<ReadonlyArray<Player>>('players');
+export const selectPlayers = createFeatureSelector<ReadonlyArray<Player>>('players');
 
 export const selectCards = createFeatureSelector<ReadonlyArray<PossessedCard>>('possessedCard');
+
+
+
+export const selectDealer = () =>
+  createSelector(selectPlayers, (players) => {
+    return players.find(
+      (player:Player) =>
+        player.name === "Mr.House"
+    )
+  }
+  );
+
 
 export const selectPlayerById = (id: number) =>
   createSelector(selectPlayers, (playersSelected) => playersSelected[id]);
@@ -35,13 +47,6 @@ export const selectPossessedCards = (playerId: number) =>
     )
   )
 
-export const selectDealer = () =>
-  createSelector(selectPlayers, (players) =>
-    players.find(
-      player =>
-        player.name === "Mr.House"
-    )
-  )
 
 
 
